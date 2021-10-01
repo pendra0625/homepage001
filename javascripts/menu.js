1df2ca820547ea6a250d9aa797734781e69dd41c
pendra0625 <pendora0625@gmail.com>
2021-10-02 05:30:20 +0900

    日記とmenu.jsの追加

---
+++ javascripts/menu.js
@@ -0,0 +1,10 @@
+document.write("<ul>");
+document.write("<li><a href='index.html'>home</a></li>");
+document.write("<li><a href='about.html'>about</a></li>");
+document.write("<li class='uchikeshi'>gallery</li>");
+document.write("<li class='uchikeshi'>how to</li>");
+document.write("<li><a href='diary.html'>diary</a></li>");
+document.write("<li class='uchikeshi'>profile</li>");
+document.write("<li class='uchikeshi'>links</li>");
+document.write("<li class='uchikeshi'>mail</li>");
+document.write("</ul>");
