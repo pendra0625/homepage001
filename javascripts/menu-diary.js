a4a669028905b142e959b9e882cc6ed92ca06ab6
pendra0625 <pendora0625@gmail.com>
2021-10-13 11:52:32 +0900

    howto test

--- javascripts/menu-diary.js
+++ javascripts/menu-diary.js
@@ -2,7 +2,7 @@ document.write("<ul>");
 document.write("<li><a href='../../../index.html'>home</a></li>");
 document.write("<li><a href='../../../about.html'>about</a></li>");
 document.write("<li><a href='../../../gallery.html'>gallery</a></li>");
-document.write("<li class='uchikeshi'>how to</li>");
+document.write("<li class='uchikeshi'><a href='../../../howto.html'>how to</a></li>");
 document.write("<li><a href='../../../diary.html'>diary</a></li>");
 document.write("<li class='uchikeshi'>profile</li>");
 document.write("<li class='uchikeshi'>links</li>");
