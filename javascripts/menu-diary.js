b6a1b03f582661714ec12260c954506cf74661c4
pendra0625 <pendora0625@gmail.com>
2021-10-16 10:32:30 +0900

    howtoのtest

--- javascripts/menu-diary.js
+++ javascripts/menu-diary.js
@@ -1,10 +1,10 @@
 document.write("<ul>");
-document.write("<li><a href='../../../index.html'>home</a></li>");
-document.write("<li><a href='../../../about.html'>about</a></li>");
-document.write("<li><a href='../../../gallery.html'>gallery</a></li>");
-document.write("<li class='uchikeshi'><a href='../../../howto.html'>how to</a></li>");
-document.write("<li><a href='../../../diary.html'>diary</a></li>");
-document.write("<li class='uchikeshi'>profile</li>");
-document.write("<li class='uchikeshi'>links</li>");
-document.write("<li class='uchikeshi'>mail</li>");
+document.write("<li id='menu'><a href='../../../index.html'>home</a></li>");
+document.write("<li id='menu'><a href='../../../about.html'>about</a></li>");
+document.write("<li id='menu'><a href='../../../gallery.html'>gallery</a></li>");
+document.write("<li class='uchikeshi' id='menu'><a href='../../../howto.html'>how to</a></li>");
+document.write("<li id='menu'><a href='../../../diary.html'>diary</a></li>");
+document.write("<li class='uchikeshi' id='menu'>profile</li>");
+document.write("<li class='uchikeshi' id='menu'>links</li>");
+document.write("<li class='uchikeshi' id='menu'>mail</li>");
 document.write("</ul>");
